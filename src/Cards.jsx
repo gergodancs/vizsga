063ad417fdae59c9bf41789c29dc17f00d3b6941
gergodancs <dancs.gergely@gmail.com>
2022-02-08 10:43:56 +0100

    milestonReady

---
+++ src/Cards.jsx
@@ -0,0 +1,24 @@
+const Cards = () => {
+  return (
+    <div className="container">
+      <div className="cards">
+        <h2>VezetékNév Keresztnév</h2>
+
+        <h6>Restaurant</h6>
+        <p>1136 Bp Kiraly utca 11.</p>
+      </div>
+      <div className="cards">
+        <h2>VezetékNév Keresztnév</h2>
+        <h6>Restaurant</h6>
+        <p>1136 Bp Kiraly utca 11.</p>
+      </div>
+      <div className="cards">
+        <h2>VezetékNév Keresztnév</h2>
+        <h6>Restaurant</h6>
+        <p>1136 Bp Kiraly utca 11.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
